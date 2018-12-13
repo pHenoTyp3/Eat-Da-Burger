fbb1a6ecb0d51d7c0db682ab89aa32dd8955e59b
nomad <t.alexalleyne@gmail.com>
2018-12-13 15:58:51 -0800

    heroku2

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers(burger_name,devoured)
+VALUES("Beef Double Cheese Burger",0);
+
+INSERT INTO burgers(burger_name,devoured)
+VALUES("Patty Melt Burger",0);
+
+INSERT INTO burgers(burger_name,devoured)
+VALUES("Mushroom & Swiss Double Cheese Burger",0);
